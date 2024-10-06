0c7b943fad5586be662b440abeef779992d9e04c
Date44 <mgura1833@gmail.com>
2024-10-06 21:47:33 +0200

    update

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,11 @@
 
 document.querySelector('#loginForm').onsubmit = async (e) => {
     e.preventDefault()
-    email = document.querySelector('#email')
+    login = document.querySelector('#email')
     password = document.querySelector('#password')
     response = await fetch('http://109.237.99.125:5000/login', {
         method: 'POST',
-        body: JSON.stringify({email: email.value, password: password.value})
+        body: JSON.stringify({login: login.value, password: password.value})
     })
         let data = await response.json()
     console.log(data)
