93029cd0594524f9cd04b2357007aa959691cdc0
Date44 <mgura1833@gmail.com>
2024-10-06 18:18:53 +0200

    update

--- static/js/base.js
+++ static/js/base.js
@@ -1,13 +1,9 @@
 async function fetchData() {
-    try {
-        const response = await fetch('http://127.0.0.1:5000/data', {
-            method: 'POST',
-        });
-        const data = await response.json();
-        console.log(data); // Здесь выводим данные
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+    const response = await fetch('http://127.0.0.1:5000/data', {
+        method: 'POST',
+    });
+    const data = await response.json();
+    console.log(data);
 }
 
 fetchData();
