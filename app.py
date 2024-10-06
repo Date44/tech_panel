f044dae0fa7f2e13e3a9ee53bd31ce81f918aa06
Date44 <mgura1833@gmail.com>
2024-10-07 01:18:00 +0200

    update

--- app.py
+++ app.py
@@ -5,61 +5,63 @@ from flask import Flask, render_template, request, jsonify, session
 from flask_cors import CORS
 import sqlite3
 
+from flask import Flask, render_template, redirect, url_for, flash
+from models import db, User
+from forms import RegistrationForm, LoginForm
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+
+
 app = Flask(__name__)
 CORS(app)
-app.secret_key = os.urandom(24)
+app.config.from_object('config.Config')
+db.init_app(app)
 
-def create_db():
-    cur.execute("CREATE TABLE Users(name UNIQUE, password VARCHAR, role STRING)")
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-def create_profile(loginUser, passwordUser):
-    data = [(loginUser, passwordUser, "user"), ]
-    cur.executemany("INSERT INTO Users VALUES(?, ?, ?)", data)
-    con.commit()
-
-
-if not os.path.exists('data.db'):
-    con = sqlite3.connect("data.db", check_same_thread=False)
-    cur = con.cursor()
-    create_db()
-else:
-    con = sqlite3.connect("data.db", check_same_thread=False)
-    cur = con.cursor()
-
-login = {
-    "213": "123",
-    "Date44": "ttt",
-    "kot_gay": "21312"
-}
 
 @app.route('/', methods=['GET'])
 async def index():
     return render_template("index.html")
 
-@app.route('/login', methods=['POST'])
-async def index1():
-    request1 = json.loads(request.data.decode("utf-8"))
-    if request1["login"] in login.keys():
-        if  request1["password"] == login[request1["login"]]:
-            request1["code"] = 200
-            session['data'] = request1
-            return jsonify(request1)
-    return jsonify({"code": 403})
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+# @app.route('/login', methods=['POST'])
+# async def index1():
+#     request1 = json.loads(request.data.decode("utf-8"))
+#     if request1["login"] in login.keys():
+#         if  request1["password"] == login[request1["login"]]:
+#             request1["code"] = 200
+#             session['data'] = request1
+#             return jsonify(request1)
+#     return jsonify({"code": 403})
 
 @app.route('/servers', methods=['GET'])
 async def servers():
-    try:
-        cur.execute("SELECT * FROM Users")
-        result = cur.fetchone()
-        print(result)
-        print(session['data'])
-        if session['data']["login"] in login.keys():
-            if  session['data']["password"] == login[session['data']["login"]]:
-                return render_template("servers.html")
-    except Exception as e:
-        print(e)
-        return f"<h1>403</h1>"
+    return render_template("servers.html")
+
 
 @app.route('/servers', methods=['POST'])
 def servers1():
