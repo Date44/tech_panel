cb1f608919422402503e355f0e5358588f11d973
Date44 <mgura1833@gmail.com>
2024-10-06 21:34:56 +0200

    update

--- static/js/base.js
+++ static/js/base.js
@@ -1,10 +1,14 @@
-async function fetchData() {
-    const response = await fetch('http://109.237.99.125:5000/data', {
+
+document.querySelector('#loginForm').onsubmit = async (e) => {
+    e.preventDefault()
+    email = document.querySelector('#email')
+    password = document.querySelector('#password')
+    response = await fetch('http://109.237.99.125:5000/login', {
         method: 'POST',
-    });
-    const data = await response.json();
-    console.log(data);
+        body: JSON.stringify({email: email.value, password: password.value})
+    })
+        let data = await response.json()
+    console.log(data)
 }
 
-fetchData();
 
