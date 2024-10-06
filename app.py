21f20345b93c8e3ac9c0320270586d46c52df1b2
Date44 <mgura1833@gmail.com>
2024-10-06 15:52:12 +0200

    update

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ discord = oauth.register(
     client_id="1079473813936414822",
     client_secret="pdOQZ4h_NcAgq0n6BXAD6iSjejJviqIB",
     access_token_url='https://discord.com/api/oauth2/token',
-    authorize_url='https://discord.com/api/oauth2/authorize?client_id=1079473813936414822&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fauthorize%2Fcallback&scope=identify',
+    authorize_url='https://discord.com/api/oauth2/authorize',
     authorize_params=None,
     client_kwargs={'scope': 'identify email'},
     userinfo_endpoint='https://discord.com/api/users/@me',
-    redirect_uri="http://195.208.172.233:5000/authorize/callback",
+    redirect_uri="http://109.237.99.125:5000/authorize/callback",
 )
 
 
