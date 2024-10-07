ead1844d0cfe967e908a536663c733b89b6c3dd0
Date44 <mgura1833@gmail.com>
2024-10-07 02:02:50 +0200

    update

--- app.py
+++ app.py
@@ -12,8 +12,6 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_object('config.Config')
 db.init_app(app)
-with app.app_context():
-    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
