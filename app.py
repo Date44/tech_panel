3d63673d8ad8e56eee7b6cff923e071992b81a93
Date44 <mgura1833@gmail.com>
2024-10-07 02:55:18 +0200

    update

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_object('config.Config')
 db.init_app(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -31,7 +33,7 @@ async def index():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, password=form.password.data)
+        user = User(username=form.username.data, password=form.password.data, role=request.form.get('role', 'user'))
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
@@ -48,6 +50,11 @@ def login():
         else:
             pass
     return render_template('login.html', form=form)
+
+@app.route('/admin')
+@role_required('admin')  # Только для администраторов
+def admin():
+    return "Welcome to the admin page!"
 
 
 @app.route('/logout')
--- models.py
+++ models.py
@@ -7,4 +7,5 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(50), nullable=False, default='user')
     
